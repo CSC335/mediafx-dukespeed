3ab7547a06a62aa52d525ffb0522b68b46a75589
dukespeed <duke@speedkin.com>
2023-03-01 16:59:44 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -1,7 +1,7 @@
 package PlaySongDemo;
 
 /**
- * This code will play any song assuming that file is in folder songfiles. 
+ * This code will play any song assuming that file is in folder song files. 
  * 
  * Programmer Rick Mercer
  */
@@ -28,10 +28,12 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
-    pane.setCenter( new Label(path));
+    // I, Duke Speed, changed the song to be played before pushing to GitHub
+    String path = "songfiles/DanseMacabreViolinHook.mp3";
+    // And swapped a few lines, in addition to changing a few typos.
     playASong(path);
     // Put the pane in a sized Scene and show the GUI
+    pane.setCenter( new Label(path));
     Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
     stage.setScene(scene);
     // Don't forget to show the running app:
---
+++ MediaFX/src/module-info.java
@@ -0,0 +1,6 @@
+module MediaFX {
+	exports PlaySongDemo;
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.media;
+}
